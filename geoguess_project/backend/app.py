aff1f8b9c3a26ad0befe6b0c8ea9f2a2a4cab93c
thecrusticroc <38624499+thecrusticroc@users.noreply.github.com>
2025-07-26 17:53:07 +0200

    Fixed Data Size

--- geoguess_project/backend/app.py
+++ geoguess_project/backend/app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
+app.config['MAX_CONTENT_LENGTH'] = 30 * 1024 * 1024 # 30MB
 CORS(app)
 
 # Coordinate Dictionary for countries (Hardcoded country locations)
